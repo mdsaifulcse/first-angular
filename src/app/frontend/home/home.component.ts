b3e568baa831749d9049677762738bbad431f357
mdsaifulcse <adeptsaiful@gmail.com>
2022-02-14 23:26:10 +0600

    Feat: Add bootstrap and implement bootstrap modal

--- src/app/frontend/home/home.component.ts
+++ src/app/frontend/home/home.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  modalbody={'header':'Modal Header','body':'Modal Body Content'}
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  displayStyle='none';
+  openModal(){
+    this.displayStyle='block';
+  }
+
+  closeModal(){
+    this.displayStyle='none';
   }
 
 }
